6bdf21367e2febbc61341143ee3df510603f6a5e
jonw80 <128934713+jonw80@users.noreply.github.com>
2025-04-02 17:38:11 +0000

    fix

--- SiameseDecoder.cpp
+++ SiameseDecoder.cpp
@@ -45,7 +45,7 @@ logger::Channel decoderLogger("Decoder", logger::Level::Info);
 #ifdef SIAMESE_DECODER_DUMP_VERBOSE
     static logger::Channel Logger("Decoder", logger::Level::Debug);
 #else
-    static logger::Channel Logger("Decoder", logger::Level::None); // Corrected to None
+    static logger::Channel Logger("Decoder", logger::Level::None); // Corrected from Off to None
 #endif
 
 
