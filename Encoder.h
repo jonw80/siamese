27a2f1c885840fbc732f9c52e47d99d3c7f8f46e
jonw80 <128934713+jonw80@users.noreply.github.com>
2025-04-03 23:51:16 -0400

    Update Encoder.h

--- Encoder.h
+++ Encoder.h
@@ -1,19 +1,37 @@
-#ifndef ENCODER_H
-#define ENCODER_H
+#ifndef SIAMESE_ENCODER_H
+#define SIAMESE_ENCODER_H
 
-#include "SiameseTypes.h"
+#include "siamese.h"
+#include <vector>
+#include <cstdint>
 
 namespace siamese {
 
 class Encoder {
 public:
     Encoder();
+
+    // Add a packet to the encoder's working set
     bool Add(SiameseOriginalPacket& packet);
+
+    // Re-add a packet that may have already been sent
     bool Retransmit(SiameseOriginalPacket& packet);
+
+    // Generate a FEC (recovery) packet
     bool GetRecoveryPacket(unsigned int id, unsigned char* buffer, unsigned int bufferSize, unsigned int& packetSize);
+
+    // Inform encoder of which packets have been received by the decoder
     bool Acknowledge(const unsigned char* data, unsigned int size, unsigned int& acknowledgedCount);
+
+private:
+    struct StoredPacket {
+        std::vector<uint8_t> data;
+        unsigned int packetNum;
+    };
+
+    std::vector<StoredPacket> packets;
 };
 
 } // namespace siamese
 
-#endif // ENCODER_H
+#endif // SIAMESE_ENCODER_H
